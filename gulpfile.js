05a957753af3eee78e340ce460ded353662b4533
Vinicius Ribeiro <hello@vncsrbro.com>
2020-08-23 21:58:04 -0300

    :wrench: Updated gulp to serve minified html

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,17 @@ var prefix = require("gulp-autoprefixer");
 var imageMin = require("gulp-imagemin");
 var browserSync = require("browser-sync").create();
 var sass = require("gulp-sass");
+var ghPages = require("gulp-gh-pages");
+
+const htmlmin = require("gulp-htmlmin");
+
+// Minify html
+gulp.task("minify", () => {
+  gulp
+    .src("index.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("dist"));
+});
 
 // SCRIPTS TASK
 gulp.task("scripts", function () {
@@ -17,6 +28,11 @@ gulp.task("scripts", function () {
     .src("src/js/**/*.js")
     .pipe(uglify().on("error", gulpUtil.log))
     .pipe(gulp.dest("dist/js"));
+});
+
+// Deploy
+gulp.task("deploy", function () {
+  return gulp.src("./dist/**/*").pipe(deploy());
 });
 
 // Image Minification
@@ -42,13 +58,14 @@ gulp.task("styles", function () {
     .pipe(browserSync.stream());
 });
 
-gulp.task("serve", ["scripts", "styles"], function () {
+gulp.task("serve", ["scripts", "styles", "minify"], function () {
   browserSync.init({
-    server: "./",
+    server: "dist",
   });
 
   gulp.watch("src/js/**/*.js", ["scripts"]);
   gulp.watch("src/sass/**/*.scss", ["styles"]);
+  gulp.watch("./index.html", ["minify"]);
   gulp.watch("./*.html").on("change", browserSync.reload);
 });
 
